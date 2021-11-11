f43f6ef6d4498f755dd67141e24707dcfd8a8e33
YuliaGV <74123271+YuliaGV@users.noreply.github.com>
2021-11-11 00:48:44 +0000

    PYuliana
    Presentación Yuliana Gaviria

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Yuliana } from './Presentaciones/Yuliana';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <h1 className="App-title">Hola, somos Code Space</h1>
+      <Yuliana />
+    </div>
+  );
+}
+
+export default App;
